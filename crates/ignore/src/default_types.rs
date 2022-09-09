332ac4cfbb81c545cbacdcc9ec1749d44e6938a8
edam <tim@ed.am>
2022-09-09 22:15:59 +0100

    added v support to types (vlang.io)

--- crates/ignore/src/default_types.rs
+++ crates/ignore/src/default_types.rs
@@ -252,6 +252,7 @@ pub const DEFAULT_TYPES: &[(&str, &[&str])] = &[
     ("twig", &["*.twig"]),
     ("txt", &["*.txt"]),
     ("typoscript", &["*.typoscript", "*.ts"]),
+    ("v", &["*.v"]),
     ("vala", &["*.vala"]),
     ("vb", &["*.vb"]),
     ("vcl", &["*.vcl"]),
